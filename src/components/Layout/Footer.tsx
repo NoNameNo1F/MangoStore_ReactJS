f23878ff7625dc5ff0756befcf6c168301da177c
20127670 <nonameno1f@gmail.com>
2024-05-21 14:58:06 +0700

    feat: Add Layout component with Header and Footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer fixed-bottom text-center p-3 db-dark text-white">
+      &copy; Made with <i className="bi bi-heart-fill"></i> By NoName
+    </div>
+  );
+}
+
+export default Footer;
