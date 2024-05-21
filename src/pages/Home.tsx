adb49e398e5ff930d33891b073f2a6d33ab895ea
20127670 <nonameno1f@gmail.com>
2024-05-21 14:55:26 +0700

    feat(ui): add Home,MenuDetails and 404 page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MenuItemList } from "../components/page/MenuItems";
+
+function Home() {
+  return (
+    <div>
+      <div className="container p-2">
+        <MenuItemList />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import NotFound from "./NotFound";
+import MenuItemDetails from "./MenuItemDetails";
+export { Home, NotFound, MenuItemDetails };
