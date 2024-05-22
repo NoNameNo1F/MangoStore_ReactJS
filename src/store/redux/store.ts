b98912ddcf116c80635069b7af09ba508817bbab
20127670 <nonameno1f@gmail.com>
2024-05-22 23:55:40 +0700

    feat: Add slice ShoppingCartItem to update quantity and remove cartitem.

---
+++ src/store/redux/store.ts
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { menuItemReducer } from "./menuItemSlice";
+import { menuItemApi, shoppingCartApi } from "../../apis/";
+import { shoppingCartReducer } from "./shoppingCartSlice";
+
+const store = configureStore({
+  reducer: {
+    menuItemStore: menuItemReducer,
+    shoppingCartStore: shoppingCartReducer,
+    [menuItemApi.reducerPath]: menuItemApi.reducer,
+    [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(menuItemApi.middleware)
+      .concat(shoppingCartApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
