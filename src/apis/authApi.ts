53fd7e020ffbfc3dc03ab754f4bf438c0a36ef14
20127670 <nonameno1f@gmail.com>
2024-05-24 23:12:19 +0700

    feat(ui): adding Auth Api RTK

---
+++ src/apis/authApi.ts
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://localhost:7670/api/",
+  }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (userData) => ({
+        url: "Auth/register",
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: userData,
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (userCredentials) => ({
+        url: "Auth/login",
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: userCredentials,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterUserMutation, useLoginUserMutation } = authApi;
+export default authApi;
